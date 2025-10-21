b89ab64cece879f0dcf782439a280bc03c137c15
daniel muniz <munidaniel@gmail.com>
2025-10-20 22:09:53 -0300

    bugfix: fixed database race condition issue

--- application-service/src/models/mysql/settings/connection.py
+++ application-service/src/models/mysql/settings/connection.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from .base_url import DB_URL
 
 class DBConnectionHandler:
     def __init__(self) -> None:
         self.__connection_string = DB_URL
         self.__engine = None
-        self.session = None
+        self.__session_factory = None
 
     def connect_to_db(self):
         self.__engine = create_engine(
@@ -17,18 +17,28 @@ class DBConnectionHandler:
             pool_size=10,
             max_overflow=20
         )
+        session_maker = sessionmaker(
+            bind=self.__engine,
+            expire_on_commit=False
+        )
+        self.__session_factory = scoped_session(session_maker)
 
     def get_engine(self):
         return self.__engine
 
+    @property
+    def session(self):
+        return self.__session_factory()
+
     def __enter__(self):
-        session_maker = sessionmaker(bind=self.__engine)
-        self.session = session_maker()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if exc_type is not None:
-            self.session.rollback()
-        self.session.close()
+        try:
+            if exc_type is not None:
+                self.session.rollback()
+            self.session.close()
+        finally:
+            self.__session_factory.remove()
 
 db_connection_handler = DBConnectionHandler()
